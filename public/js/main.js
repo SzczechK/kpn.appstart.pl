886e9f5ff922ec7e4c59172f9fdfafca216a4142
Krzysztof Szczech <krystof@onet.com.pl>
2023-10-13 09:56:27 +0200

    ver. 1.0.1

--- public/js/main.js
+++ public/js/main.js
@@ -1,48 +1,37 @@
 const pickArray = ['rock', 'papper', 'scissors'];
+const selections = document.querySelectorAll('.pick-box img');
 const cpuChatWin = [];
 const cpuChatLoss = [];
-const chooseRock = document.querySelector('#item-rock');
-const choosePapper = document.querySelector('#item-papper');
-const chooseScissors = document.querySelector('#item-scissors');
-const winField = document.querySelector('.win');
-const lossField = document.querySelector('.loss');
-const drawField = document.querySelector('.draw');
-const bestWinField = document.querySelector('.best-win');
-const winInaRowField = document.querySelector('.win-in-a-row');
-const resultField = document.querySelector('.result-text');
-let win = 0;
-let loss = 0;
-let draw = 0;
-let winsInaRow = 0;
-let bestWinsNumber = 0;
 
-chooseRock.addEventListener('click', () => {
-  showPlayerChoice(chooseRock.dataset.type);
-  let result = checkWinner(cpuPick(), chooseRock.dataset.type);
-  showResult(result);
-});
+const gameSummary = {
+  win: 0,
+  loss: 0,
+  draw: 0,
+  winsInaRow: 0,
+  bestWinsNumber: 0,
+};
 
-choosePapper.addEventListener('click', () => {
-  showPlayerChoice(choosePapper.dataset.type);
-  let result = checkWinner(cpuPick(), choosePapper.dataset.type);
-  showResult(result);
-});
+const game = {
+  playerSelection: null,
+  cpuSelection: null,
+};
 
-chooseScissors.addEventListener('click', () => {
-  showPlayerChoice(chooseScissors.dataset.type);
-  let result = checkWinner(cpuPick(), chooseScissors.dataset.type);
-  showResult(result);
-});
+selections.forEach((selection) =>
+  selection.addEventListener('click', selectHand)
+);
 
 // Function here
 
-function cpuPick() {
-  let cpuPick = pickArray[Math.floor(Math.random() * pickArray.length)];
-  return cpuPick;
+function selectHand(e) {
+  game.cpuSelection = pickArray[Math.floor(Math.random() * pickArray.length)];
+  game.playerSelection = e.target.dataset.type;
+  showPcChoice(game.cpuSelection);
+  showPlayerChoice(game.playerSelection);
+  let gameResult = checkWinner(game.cpuSelection, game.playerSelection);
+  showResult(gameResult);
 }
 
 function checkWinner(pc, player) {
-  showPcChoice(pc);
   let result = '';
   if (player == 'rock') {
     switch (pc) {
@@ -93,27 +82,33 @@ function checkWinner(pc, player) {
 }
 
 function showResult(result) {
+  const winField = document.querySelector('.win');
+  const lossField = document.querySelector('.loss');
+  const drawField = document.querySelector('.draw');
+  const bestWinField = document.querySelector('.best-win');
+  const winInaRowField = document.querySelector('.win-in-a-row');
+  const resultField = document.querySelector('.result-text');
   if (result == 'win') {
-    win++;
-    winField.textContent = win;
-    winsInaRow++;
-    winInaRowField.textContent = winsInaRow;
-    if (winsInaRow > bestWinsNumber) {
-      bestWinsNumber = winsInaRow;
-      bestWinField.textContent = bestWinsNumber;
+    gameSummary.win++;
+    winField.textContent = gameSummary.win;
+    gameSummary.winsInaRow++;
+    winInaRowField.textContent = gameSummary.winsInaRow;
+    if (gameSummary.winsInaRow > gameSummary.bestWinsNumber) {
+      gameSummary.bestWinsNumber = gameSummary.winsInaRow;
+      bestWinField.textContent = gameSummary.bestWinsNumber;
     }
     resultField.style.color = 'green';
     resultField.textContent = 'Wygrana!';
   } else if (result == 'loss') {
-    loss++;
-    lossField.textContent = loss;
-    winsInaRow = 0;
-    winInaRowField.textContent = winsInaRow;
+    gameSummary.loss++;
+    lossField.textContent = gameSummary.loss;
+    gameSummary.winsInaRow = 0;
+    winInaRowField.textContent = gameSummary.winsInaRow;
     resultField.style.color = 'red';
     resultField.textContent = 'Niestety... przegrana';
   } else {
-    draw++;
-    drawField.textContent = draw;
+    gameSummary.draw++;
+    drawField.textContent = gameSummary.draw;
     resultField.style.color = 'black';
     resultField.textContent = 'Remis';
   }
